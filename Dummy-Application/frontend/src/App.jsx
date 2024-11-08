be2de671c6142d5a87f0dd60ed26a4fc51f87042
Mandar <mandargurjar51@gmail.com>
2024-11-08 16:00:11 +0530

    Hosted Dummy application(frontend,backedn)

---
+++ Dummy-Application/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import ResearchComponent from '../components/ResearchComponent'
+
+function App() {
+
+  return (
+    <>
+    <ResearchComponent/>
+    </>
+  )
+}
+
+export default App
