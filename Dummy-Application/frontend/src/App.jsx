1fa4c805e7f49d8d1996c5ddeb648b5df9dcb2af
Mandar <mandargurjar51@gmail.com>
2024-11-09 11:49:21 +0530

    Added basic frontend

--- Dummy-Application/frontend/src/App.jsx
+++ Dummy-Application/frontend/src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import './App.css'
-import ResearchComponent from '../components/ResearchComponent'
+import React from "react";
+import ResearchComponent from "../src/components/ResearchComponent";  // Import the ResearchComponent
+import styles from '../src/styles/research.module.css'
 
-function App() {
-
+const App = () => {
   return (
-    <>
-    <ResearchComponent/>
-    </>
-  )
-}
+    <div>
+      <h1 className={styles.h1}>Research Document Generator</h1>
+      <ResearchComponent />  {/* Use the ResearchComponent */}
+    </div>
+  );
+};
 
-export default App
+export default App;
